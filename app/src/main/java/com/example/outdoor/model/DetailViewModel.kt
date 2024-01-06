79db54c3a01c86280e2d209d839ae1699420e5f8
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2024-01-06 18:17:21 +0700

    Membuat val PenyewaRepository pada DetailViewModel.kt

--- app/src/main/java/com/example/outdoor/model/DetailViewModel.kt
+++ app/src/main/java/com/example/outdoor/model/DetailViewModel.kt
@@ -1,3 +1,9 @@
 package com.example.outdoor.model
 
-class DetailViewModel (){}
+import androidx.lifecycle.SavedStateHandle
+import com.example.outdoor.repositori.PenyewaRepository
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val penyewaRepository: PenyewaRepository
+){}
