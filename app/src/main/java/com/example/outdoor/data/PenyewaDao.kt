4535546e642cdb3532a1806798928af16217b08b
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2024-01-06 17:54:06 +0700

    Membuat fun update pada interface PenyewaDao

--- app/src/main/java/com/example/outdoor/data/PenyewaDao.kt
+++ app/src/main/java/com/example/outdoor/data/PenyewaDao.kt
@@ -2,8 +2,12 @@ package com.example.outdoor.data
 
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 interface PenyewaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(penyewa: Penyewa)
+
+    @Update
+    suspend fun update(penyewa: Penyewa)
 }
