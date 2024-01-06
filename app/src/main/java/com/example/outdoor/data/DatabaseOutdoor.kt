1bc9f04d0c5a480f6c184f0abdee00b29751dd4e
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2024-01-06 18:05:18 +0700

    Membuat companion object pada abstract class DatabaseOutdoor

--- app/src/main/java/com/example/outdoor/data/DatabaseOutdoor.kt
+++ app/src/main/java/com/example/outdoor/data/DatabaseOutdoor.kt
@@ -1,7 +1,23 @@
 package com.example.outdoor.data
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
 
 abstract class DatabaseOutdoor : RoomDatabase(){
     abstract fun penyewaDao(): PenyewaDao
+    companion object {
+        @Volatile
+        private var Instance: DatabaseOutdoor? = null
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getDatabase(context: Context): DatabaseOutdoor {
+            return (Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, DatabaseOutdoor::class.java, "adventure_mountain")
+                    .build()
+                    .also { Instance = it }
+            })
+        }
+    }
 }
