c9e09c01c161a4d4f63276a1cf377485d27eb932
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2024-01-06 17:31:13 +0700

    Menambahkan val Barang Pada file Barang.kt

--- app/src/main/java/com/example/outdoor/data/Barang.kt
+++ app/src/main/java/com/example/outdoor/data/Barang.kt
@@ -1,4 +1,13 @@
 package com.example.outdoor.data
 
+import com.example.outdoor.R
+
 object Barang {
+    val barang = listOf(
+        R.string.tenda,
+        R.string.jaket,
+        R.string.sepatu,
+        R.string.sarung_tangan,
+        R.string.sb
+    )
 }
