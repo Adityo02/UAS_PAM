2eb832257e80891708a036551c7112efc73eb2d4
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2024-01-08 18:46:44 +0700

    Membuat private val penyewaId dan dataclass ItemDetailUiState pada DetailViewModel.kt

--- app/src/main/java/com/example/outdoor/model/DetailViewModel.kt
+++ app/src/main/java/com/example/outdoor/model/DetailViewModel.kt
@@ -1,9 +1,39 @@
 package com.example.outdoor.model
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.outdoor.repositori.PenyewaRepository
+import com.example.outdoor.ui.halaman.RouteDetail
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
     private val penyewaRepository: PenyewaRepository
-){}
+) : ViewModel() {
+    private val penyewaId: Int = checkNotNull(savedStateHandle[RouteDetail.sewaIdArg])
+    val uiState: StateFlow<ItemDetailUiState> =
+        penyewaRepository.getPenyewaStream(penyewaId).filterNotNull()
+            .map { ItemDetailUiState(penyewaBio = it.toDetailPenyewa()) }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailUiState()
+            )
+
+    suspend fun deleteItem() {
+        penyewaRepository.deletePenyewa(uiState.value.penyewaBio.toPenyewa())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+data class ItemDetailUiState(
+    val outOfStock: Boolean = true,
+    val penyewaBio: PenyewaBio = PenyewaBio()
+)
