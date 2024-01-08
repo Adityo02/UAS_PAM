03233bd0e124378f935069f1d4a4a5d3616ffb84
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2024-01-08 18:34:53 +0700

    Membuat object RouteHome pada HalamanHome.kt

---
+++ app/src/main/java/com/example/outdoor/ui/halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.outdoor.ui.halaman
+
+import com.example.outdoor.R
+import com.example.outdoor.navigasi.RouteMap
+
+object RouteHome : RouteMap {
+    override val route = "home"
+    override val tittleRes = R.string.app_name
+}
