31bdb2b83af5932bb1a2b76d08b589771c5742b5
Adityo02 <adityo.arya.ft21@mail.umy.ac.id>
2024-01-08 18:59:31 +0700

    Menambahkan fun fun AdventureMountain, fun HostNavigasi pada PengelolaHalaman.kt

--- app/src/main/java/com/example/outdoor/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/outdoor/navigasi/PengelolaHalaman.kt
@@ -9,6 +9,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.outdoor.ui.halaman.DetailScreen
+import com.example.outdoor.ui.halaman.EntryPesananScreen
+import com.example.outdoor.ui.halaman.HomeScreen
+import com.example.outdoor.ui.halaman.ItemEditScreen
+import com.example.outdoor.ui.halaman.RouteDetail
+import com.example.outdoor.ui.halaman.RouteEdit
+import com.example.outdoor.ui.halaman.RouteEntry
+import com.example.outdoor.ui.halaman.RouteHome
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,4 +46,53 @@ fun AdventureAppBar(
             }
         }
     )
+}
+
+@Composable
+fun AdventureMountain(navController: NavHostController = rememberNavController()) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = RouteHome.route) {
+        composable(RouteHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(RouteEntry.route) },
+                onDetailClick = { itemId -> navController.navigate("${RouteDetail.route}/$itemId") }
+            )
+        }
+        composable(RouteEntry.route) {
+            EntryPesananScreen(navigateBack = { navController.popBackStack() })
+        }
+        composable(
+            RouteDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(RouteDetail.sewaIdArg) {
+                    type = NavType.IntType
+                },
+            ),
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt(RouteDetail.sewaIdArg)
+            itemId?.let {
+                DetailScreen(
+                    navigateToEditItem = { navController.navigate("${RouteEdit.route}/$it") },
+                    navigateBack = { navController.popBackStack() })
+            }
+        }
+        composable(
+            RouteEdit.routeWithArgs,
+            arguments = listOf(navArgument(RouteEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            ItemEditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() })
+
+        }
+    }
 }
